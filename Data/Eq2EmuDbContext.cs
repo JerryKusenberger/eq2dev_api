4275637496dd53377940aa6d537c6df879808937
Jerry Kusenberger <gpdrew@gmail.com>
2023-07-14 13:08:50 -0500

    configure database connection

---
+++ Data/Eq2EmuDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eq2dev_api.Data
+{
+    public partial class Eq2EmuDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Eq2EmuDbContext(DbContextOptions<Eq2EmuDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using eq2dev_api.Data;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("MariaDbConnectionString");
+
+var serverVersion = new MySqlServerVersion(ServerVersion.AutoDetect(connectionString));
+
+builder.Services.AddDbContext<Eq2EmuDbContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
 
 var app = builder.Build();
 
