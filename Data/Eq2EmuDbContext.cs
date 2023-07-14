7dda4449720d4b8d48b20dd3cc2fa4c071a9d038
Jerry Kusenberger <gpdrew@gmail.com>
2023-07-14 18:39:32 -0500

    Update Eq2EmuDbContext.cs

--- Data/Eq2EmuDbContext.cs
+++ Data/Eq2EmuDbContext.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.EntityFrameworkCore;
 
-namespace eq2dev_api.Data
-{
-    public partial class Eq2EmuDbContext : Microsoft.EntityFrameworkCore.DbContext
-    {
-        public Eq2EmuDbContext(DbContextOptions<Eq2EmuDbContext> options)
-            : base(options)
-        {
-        }
-    }
-}
+//namespace eq2dev_api.Data
+//{
+//    public partial class Eq2EmuDbContext : Microsoft.EntityFrameworkCore.DbContext
+//    {
+//        public Eq2EmuDbContext(DbContextOptions<Eq2EmuDbContext> options)
+//            : base(options)
+//        {
+//        }
+//    }
+//}
