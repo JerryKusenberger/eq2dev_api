a2c587a6c9ff79e4ec229a3ce6c1bf37c78f4934
Jerry Kusenberger <gpdrew@gmail.com>
2023-07-13 21:46:58 -0500

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eq2dev_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            return Ok("success");
+        }
+    }
+}
